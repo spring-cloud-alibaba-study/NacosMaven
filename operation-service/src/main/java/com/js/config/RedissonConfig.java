bd885ee6f33439450ccb5b1328712749ccf86c90
jiangshuang <2545251075@qq.com>
2021-07-16 17:54:48 +0800

    fix -> 暂时不好使待调整

---
+++ operation-service/src/main/java/com/js/config/RedissonConfig.java
@@ -0,0 +1,26 @@
+package com.js.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author
+ * @date 2019-07-26
+ */
+@Configuration
+public class RedissonConfig {
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+
+        RedissonClient redisson = Redisson.create(config);
+
+        return redisson;
+    }
+
+}
--- operation-service/src/main/java/com/js/controller/TestController.java
+++ operation-service/src/main/java/com/js/controller/TestController.java
@@ -2,9 +2,13 @@ package com.js.controller;
 
 import com.js.dubbo.TestDubboService;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @program: NacosMaven
@@ -19,9 +23,28 @@ public class TestController {
     @Autowired
     private TestDubboService testDubboService;
 
+    @Autowired
+    private RedissonClient redissonClient;
+
     @GetMapping("/test")
     public String getString() {
         log.info("进入消费者");
-        return testDubboService.sayHello("jiangshuang");
+        RLock lock = redissonClient.getLock("test1");
+        try {
+            Boolean temp = lock.tryLock(100, 200, TimeUnit.SECONDS);
+            log.info("当前锁的状态{}",lock.isLocked());
+            log.info("获取分布式锁的结果为{}", temp);
+            if (temp) {
+                wait(9000);
+                return testDubboService.sayHello("jiangshuang");
+            }
+            log.warn("获取分布式锁失败");
+
+        } catch (Exception e) {
+            log.info("调用dubbo出现异常");
+        } finally {
+            lock.unlock();
+        }
+        return null;
     }
 }
