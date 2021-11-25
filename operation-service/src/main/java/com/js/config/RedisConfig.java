40cf8bc739bc407b40656cd7d83069b1685ac765
姜爽 <2545251075@qq.com>
2021-11-25 21:48:04 +0800

    fix

--- operation-service/src/main/java/com/js/config/RedisConfig.java
+++ operation-service/src/main/java/com/js/config/RedisConfig.java
@@ -3,14 +3,20 @@ package com.js.config;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.io.IOException;
 
 /**
  * @Author: 姜爽
@@ -42,6 +48,13 @@ public class RedisConfig {
         template.setHashValueSerializer(serializer);
         template.afterPropertiesSet();
         return template;
+    }
+
+    @Bean(value = "redissonClient")
+    public RedissonClient redissonClient() throws IOException {
+        RedissonClient redisson = Redisson.create(
+                Config.fromYAML(new ClassPathResource("application-redisson.yml").getInputStream()));
+        return redisson;
     }
 
 
