85603e45dc2201d840afac8fb4c2983367c18c4b
姜爽 <jiangshuang@wejoydata.com>
2021-12-01 14:24:24 +0800

    挂载springcloud 新版本默认无法引入rabbin 需要单独引入处理

--- operation-service/src/main/java/com/js/feignclient/UserTestProxy.java
+++ operation-service/src/main/java/com/js/feignclient/UserTestProxy.java
@@ -4,7 +4,7 @@ package com.js.feignclient;
 import com.js.api.TestControllerApi;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(value = "user-service")
+@FeignClient(name = "user-service")
 public interface UserTestProxy extends TestControllerApi {
 
 }
