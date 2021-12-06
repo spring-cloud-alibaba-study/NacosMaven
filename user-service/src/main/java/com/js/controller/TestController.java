ff9c673050632e11267179c3d1f8535279e44530
姜爽 <jiangshuang@wejoydata.com>
2021-12-06 11:02:45 +0800

    升级SpringCloud版本号

--- user-service/src/main/java/com/js/controller/TestController.java
+++ user-service/src/main/java/com/js/controller/TestController.java
@@ -1,12 +1,17 @@
 package com.js.controller;
 
 import com.js.api.TestControllerApi;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class TestController implements TestControllerApi {
+
     @Override
-    public String test(String msg) {
+    public String test(@RequestParam("msg") String msg) {
+        log.info("请求进入");
         return "YES THIS iS USER_SERVICE";
     }
 }
