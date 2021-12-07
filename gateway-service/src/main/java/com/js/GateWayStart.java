e79ff111f362bda7d29e307d372ceefd4596c2e6
姜爽 <jiangshuang@wejoydata.com>
2021-12-07 13:00:39 +0800

    zuul网关没有包涵统一异常拦截

--- gateway-service/src/main/java/com/js/GateWayStart.java
+++ gateway-service/src/main/java/com/js/GateWayStart.java
@@ -1,9 +1,11 @@
 package com.js;
 
+import com.js.util.StartLogo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * @Description 网关启动类
@@ -12,38 +14,17 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @Slf4j
 @EnableDiscoveryClient
+@EnableZuulProxy
 @SpringBootApplication
 public class GateWayStart {
     public static void main(String[] args) {
         try {
             SpringApplication.run(GateWayStart.class, args);
-            log.info(print());
+            log.info(StartLogo.print());
+            log.info("项目启动成功");
         } catch (Exception e) {
             log.info("项目启动失败");
         }
-    }
-    public static synchronized String print() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("\n");
-        sb.append("                   _ooOoo_\n");
-        sb.append("                  o8888888o\n");
-        sb.append("                  88\" . \"88\n");
-        sb.append("                  (| -_- |)\n");
-        sb.append("                  O\\  =  /O\n");
-        sb.append("               ____/`---'\\____\n");
-        sb.append("             .'  \\\\|     |//  `.\n");
-        sb.append("            /  \\\\|||  :  |||//  \\ \n");
-        sb.append("           /  _||||| -:- |||||-  \\ \n");
-        sb.append("           |   | \\\\\\  -  /// |   |\n");
-        sb.append("           | \\_|  ''\\---/''  |   |\n");
-        sb.append("           \\  .-\\__  `-`  ___/-. /\n");
-        sb.append("         ___`. .'  /--.--\\  `. . __\n");
-        sb.append("      .\"\" '<  `.___\\_<|>_/___.'  >'\"\".\n");
-        sb.append("     | | :  `- \\`.;`\\ _ /`;.`/ - ` : | |\n");
-        sb.append("     \\  \\ `-.   \\_ __\\ /__ _/   .-` /  /\n");
-        sb.append("======`-.____`-.___\\_____/___.-`____.-'======\n");
-        sb.append("                   `=---='\n");
-        sb.append(".....................项目启动成功...............\n");
-        return sb.toString();
+
     }
 }
