b0580e8b7a4dc6d500d44975e30a126c0ca710ab
姜爽 <jiangshuang@wejoydata.com>
2022-01-07 12:32:50 +0800

    redission的zset操作

--- operation-service/src/test/java/com/js/OperationApplicationTests.java
+++ operation-service/src/test/java/com/js/OperationApplicationTests.java
@@ -1,14 +1,28 @@
 package com.js;
 
 import org.junit.jupiter.api.Test;
+import org.redisson.api.RScoredSortedSet;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
 class OperationApplicationTests {
 
+    @Autowired
+    private RedissonClient redissonClient;
+
     @Test
     void contextLoads() {
-
+        final RScoredSortedSet<String> scoredSortedSet = redissonClient.getScoredSortedSet("test");
+        scoredSortedSet.expire(40, TimeUnit.SECONDS);
+        scoredSortedSet.addAndGetRevRank(20000000, "test3");
+        scoredSortedSet.addAndGetRevRank(50000000, "test1");
+        scoredSortedSet.addAndGetRevRank(70000000, "test2");
+        final Integer test1 = scoredSortedSet.revRank("test1") + 1;
+        System.out.println(test1);
     }
 
 }
