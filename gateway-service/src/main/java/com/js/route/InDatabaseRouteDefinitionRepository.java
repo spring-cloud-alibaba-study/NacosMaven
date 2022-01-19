c21b19ce4ce4dd4b053294b110f4a6bbb66b4a45
姜爽 <2545251075@qq.com>
2022-01-19 21:19:25 +0800

    fix 尝试网关配置持久化

---
+++ gateway-service/src/main/java/com/js/route/InDatabaseRouteDefinitionRepository.java
@@ -0,0 +1,30 @@
+package com.js.route;
+
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/***
+ * @Description: 可以通过这种方式对网关的路由进行持久化
+ * @Param
+ * @Date: 2022/1/19 9:18 PM
+ * @return
+ */
+//@Component
+public class InDatabaseRouteDefinitionRepository implements RouteDefinitionRepository {
+    @Override
+    public Flux<RouteDefinition> getRouteDefinitions() {
+        return null;
+    }
+
+    @Override
+    public Mono<Void> save(Mono<RouteDefinition> route) {
+        return null;
+    }
+
+    @Override
+    public Mono<Void> delete(Mono<String> routeId) {
+        return null;
+    }
+}
